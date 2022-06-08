e8cd7c55518f9077fa24103b101f3ac222f956ac
kreuter92 <kristina.reuter@outlook.de>
2022-06-08 17:58:10 +0200

    versionierung und hilfe hinzugefügt

--- lineare_suche.c
+++ lineare_suche.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int suche(int arr[], int x, int laenge)
+const char VERSIONSNR[] = "0.1";
+
+int suche(int liste[], int suchwert, int laenge)
 {
    for (int i = 0; i < laenge; i++){
-	   if (arr[i] == x){
+	   if (liste[i] == suchwert){
 		   return i;
 	   }
    }
    return -1;
 }
 
-int main(void)
+int main(int argc, char* argv[])
 {
-    int arr[] = {5, 10, 15, 20, 25};
-    int x = 15;
-    int laenge = sizeof arr / sizeof arr[0];
-    int ergebnis = suche(arr, x, laenge);
-    if (ergebnis == -1){
+    int option;
+    int liste[] = {5, 10, 15, 20, 25};
+    int suchwert = 15;
+    int ergebnis;
+
+    while((option = getopt(argc, argv, "hv"))!=-1)
+    switch(option)
+    {
+	    case 'h':
+		    printf("-h -> Hilfe aufrufen\n");
+		    printf("-v -> Version anzeigen\n");
+		    exit(0);
+	    case 'v':
+		    printf("Version %s\n", VERSIONSNR);
+		    exit(0);
+	    default:
+		    printf("Etwas ist schiefgegangen!\n");
+		    printf("-h hilft dir weiter\n");
+		    exit(0);
+    }
+
+    if ((ergebnis = suche(liste, suchwert, sizeof liste/ sizeof liste[0])) == -1){
 	    printf("Kein Ergebnis gefunden\n");
     }
     else{
